ffecefca1b86343ef96194e68f270944e283c42f
BenjaminGott <benjamin.gottrant@gmail.com>
2025-01-10 16:28:44 +0100

    f

--- main.py
+++ main.py
@@ -15,7 +15,27 @@ RECT_WIDTH = WIDTH // COLS
 
 OUTLINE_COLOR = (187, 173, 160)
 OUTLINE_THICKNESS = 10
+BACKROUND_COLOR = (205,192,180)
+FONT_COLOR = (199,110,101)
 
+FONT = pygame.font.SysFont("comicsans",60,bold=True)
+MOVE_VEL=20
 
+WINDOW = pygame.display.set_mode((WIDTH , HEIGHT))
+pygame.display.set_caption("2048")
 
+def main(window):
+    clock = pygame.time.Clock()
+    run=True
 
+    while run : 
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+                break
+
+    pygame.quit()      
+
+if __name__ == "__main__" : 
+    main(WINDOW)

6c4dfc447dfed3bc77cca9c7cee188b8aca97a6e
malucas <lucas.marciniak@ynov.com>
2025-01-17 11:26:29 +0100

    montecarlo IA

---
+++ montecarlo.py
@@ -0,0 +1,18 @@
+import pygame
+
+Max = 0
+Choix = None
+directions = ['left', 'right', 'up', 'down']
+
+
+for direction in directions :
+    score = 0
+    pygame.event = direction # jouer coup dans 2048
+    
+    # plateau = new plateau
+
+    for j in range(1000) :
+        #finir partie
+
+
+def montecarlo() : 
